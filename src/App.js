5dc04db9e07fef6202f1a76e7c5a7455169e53de
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-07 17:54:42 +0530

    rendered lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
-function App() {
-  return <div>Hello Ap bye bye YCP</div>;
+import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Watch", quantity: 2, packed: false },
+  { id: 4, description: "Charger", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}
