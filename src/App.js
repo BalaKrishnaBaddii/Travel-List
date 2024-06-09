3f770e5e28a1958679ce58515a64e58027754b8b
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-09 18:44:20 +0530

    lifting state up

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -23,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -32,6 +36,8 @@ function Form() {
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+    onAddItems(newItem);
+
     setDescription("");
     setQuantity(1);
   }
@@ -59,11 +65,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
